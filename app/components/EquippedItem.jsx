22ab4ae40f1fbed7f17013c2cd8e46441c0712e6
Aly Fulton <aly@obeythelime.com>
2016-12-15 20:35:34 -0500

    fix title

--- app/components/EquippedItem.jsx
+++ app/components/EquippedItem.jsx
@@ -4,7 +4,7 @@ const EquippedItem = (props) => {
   return props.isLoading === true
   ? <img src='./svg/loading.svg' alt='Loading...' />
   : <div className='item'>
-      { props.icon ? <img src={ props.icon } alt={ props.name } title={ 'Item: ' + props.rarity } className={ 'img-circle img-responsive item-resize get-inline ' + props.rarity } /> 
+      { props.icon ? <img src={ props.icon } alt={ props.name } title={ 'Item: ' + props.name } className={ 'img-circle img-responsive item-resize get-inline ' + props.rarity } /> 
         : <img src='../img/itemnotfound.png' alt={ props.name } className={'img-circle img-responsive item-resize get-inline ' + props.rarity } /> }
     </div>
 }
